8d496556f7e4a38133024086db7e7d1504034cd1
Timur Ramazanov <t.ramazanov@idfly.ru>
2016-12-30 11:50:49 +0600

    Add host option to binary call

--- lib/hanami_webpack/dev_server.rb
+++ lib/hanami_webpack/dev_server.rb
@@ -4,7 +4,7 @@ module HanamiWebpack
   module DevServer
     def start
       if HanamiWebpack::Config.using_dev_server? && HanamiWebpack::Config.inbuilt_dev_server?
-        spawn('./node_modules/.bin/webpack-dev-server')
+        spawn('./node_modules/.bin/webpack-dev-server --host=0.0.0.0')
       end
       super
     end
